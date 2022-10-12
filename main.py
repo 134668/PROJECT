3b7b2159b8d01cf2efe17b650d4646b855cc32bf
Agnes <agnesanyango30@gmail.com>
2022-10-12 12:04:11 +0300

    Created main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+#graphical representation of Madaraka estate network
+G.add_node('SportComplex')
+G.add_node('Siwaka')
+G.add_node('PH.1A')
+G.add_node('PH.1B')
+G.add_node('STC')
+G.add_node('Phase2')
+G.add_node('J1')
+G.add_node('Mada')
+G.add_node('Phase3')
+G.add_node('ParkingLot')
